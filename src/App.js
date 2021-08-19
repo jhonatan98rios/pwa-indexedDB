e4eef14d07ccd29baac8f3ef7ec6d0ec24a91fa8
Jhonatan Teixeira Rios <jhonatan.rios@the9level.com>
2021-08-18 21:10:29 -0300

    pwa test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { set, get } from 'idb-keyval';
 
 function App() {
+
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    ;(async () => {
+      let batata = await get('batata')
+
+      if (batata) {
+        setData(batata)
+
+      } else {
+        await set('batata', 'Eu sou uma batata')
+      }
+    })()
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <main>
+
+        <h2> {data} </h2>
+
+      </main>
     </div>
   );
 }
